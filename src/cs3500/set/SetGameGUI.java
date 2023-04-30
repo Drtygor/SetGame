ecfa8b9630e01bd73de8c726bf0c73e14e50eb86
Drtygor <kyeung114@gmail.com>
2023-04-30 18:44:07 -0400

    GUI

--- src/cs3500/set/SetGameGUI.java
+++ src/cs3500/set/SetGameGUI.java
@@ -1,16 +1,50 @@
+package cs3500.set;
+
+import java.io.InputStreamReader;
+
 import cs3500.set.controller.SetGameController;
-import cs3500.set.model.hw02.SetThree;
+
+import cs3500.set.controller.SetGameControllerImpl;
+import cs3500.set.model.hw02.Card;
+import cs3500.set.model.hw02.SetGameModel;
+import cs3500.set.model.hw02.SetThreeGameModel;
+import cs3500.set.view.BoardPanel;
+import cs3500.set.view.IBoardPanel;
 import cs3500.set.view.SetGameGuiView;
+import cs3500.set.view.SetGameTextView;
+import cs3500.set.view.SetGameView;
 import cs3500.set.view.SwingGuiView;
 
 
 public class SetGameGUI {
-  public static void main(String[] args) {
-    SetThree model = new SetThree();
-    SetGameGuiView view = new SwingGuiView(model);
 
-    SetGameController controller = new SetGameController(model, view);
-    controller.startGame(model.getDeck(), 3, 3);
+
+
+  public static void main(String[] args) {
+
+    SetGameModel<Card> model = new SetThreeGameModel();
+    model.startGameWithDeck(model.getCompleteDeck(),3, 3);
+
+
+    BoardPanel boardPanel = new BoardPanel(model);
+    IBoardPanel gamePanel = new GamePanel(boardPanel, model.getModelState());
+
+
+    SetGameGUIView view = new SetGameGUIView(gamePanel, model);
+    controller.playGame();
+
+
+
+
+    SetGameModel game = new SetGameModelImpl();
+    game.startGame(game.getDeck(), 3, 3);
+
+    BoardPanel boardPanel = new BoardPanel(game.getModelState());
+    ISetPanel gamePanel = new GamePanel(boardPanel, game.getModelState());
+
+    SetGameGUIView view = new SetGameGUIView(gamePanel, game);
+    view.display();
+
+
   }
-}
 }
